a9873d375e3948b2df4e8395f397fff640f5636c
sajanji15859-cmyk <sajanji15859@gmail.com>
2026-07-28 10:34:41 +0530

    Connect Brain and Memory

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ print("Memory Loaded")
 print("Research Loaded")
 print("Logger Loaded")
 print("ERA AI Ready")
+
+
+from brain import Brain
+from memory import Memory
+
+def main():
+    print("🚀 ERA AI Starting...\n")
+
+    brain = Brain()
+    brain.status()
+
+    print("\n-----------------\n")
+
+    memory = Memory()
+    memory.remember("सरफराज ERA AI बना रहा है")
+    memory.remember("ERA AI Professional Project")
+
+    memory.show()
+
+if __name__ == "__main__":
+    main()
